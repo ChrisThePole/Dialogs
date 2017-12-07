206d8a2c987a610ea805e7a085ae90042fb046b7
ChrisThePole <hewc1_16@uni.worc.ac.uk>
2017-12-07 14:04:09 +0000

    wdeqf
    efef

--- sample code/js/index.js
+++ sample code/js/index.js
@@ -23,7 +23,8 @@ $(document).on('pageinit', function() {
 function createMessage(){		
 	//phoneGap and jQueryMobile do not support toast messages directly
     //so we can add this using toast.js
-    new Toast({content: 'An example message.', duration: 1000}); 	
+    new Toast({content: "Allo y'alright?!", duration: 3000});
+    //new Toast({content: "Ow am ya?!", duration: 3000});
 }
         	
 
